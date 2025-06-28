1a3130f8158c3ed250ca93976fedf5b6dcb5d055
Tahir Mahmood <Tahirmahm123@gmail.com>
2025-06-28 19:36:47 +0500

    Updated OpenSSL

--- Package.swift
+++ Package.swift
@@ -41,7 +41,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver", from: "1.9.0"),
-        .package(url: "https://github.com/passepartoutvpn/openssl-apple", from: "3.2.105"),
+        .package(url: "https://github.com/0xBF90E913/openssl-apple", from: "3.2.110"),
 //        .package(url: "https://git.zx2c4.com/wireguard-apple", .exact: Version("1.0.15-26")),
 //        .package(url: "https://github.com/passepartoutvpn/wireguard-apple", exact: Version("1.0.17")),
         .package(url: "https://github.com/tahirmahm123/wireguard-apple", branch: "master")
